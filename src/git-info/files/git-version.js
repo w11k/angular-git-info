a76c8e976730539dd7399872e6b0ff076e4205ec
Kai Henzler <henzler@w11k.de>
2021-02-17 08:29:04 +0100

    adds eslint-disable statement to generated file

--- src/git-info/files/git-version.js
+++ src/git-info/files/git-version.js
@@ -18,6 +18,7 @@ const file = resolve(__dirname, 'src', 'environments', 'version.ts');
 writeFileSync(file,
     `// IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
 /* tslint:disable */
+/* eslint-disable */
 export const VERSION = ${JSON.stringify(gitInfo, null, 4)};
 /* tslint:enable */
 `, { encoding: 'utf-8' });
